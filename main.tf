2ca55765590b87b634eb73249a0efa823c2baaee
mitraman <ronnie.mitra@gmail.com>
2020-06-15 23:27:51 +0100

    main

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "kubernetes" {}
+provider "helm" {}
+
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "argo"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "msur"
+  chart      = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  namespace  = "argo"
+}
