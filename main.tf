a6bd3281aebd12b2d3031aa1c053b6568c6780b7
mitraman <ronnie.mitra@gmail.com>
2020-10-04 10:55:17 +0100

    reverted back to passing all values

--- main.tf
+++ main.tf
@@ -1,37 +1,14 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-data "aws_eks_cluster" "msur" {
-  name = var.kubernetes_cluster_id
-}
-
-/*
 provider "kubernetes" {
   load_config_file       = false
-  cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
   cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
   host                   = var.kubernetes_cluster_endpoint
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
     command     = "aws-iam-authenticator"
-    args        = ["token", "-i", "${var.kubernetes_cluster_name}"]  }
-
-}
-*/
-
-provider "kubernetes" {
-  load_config_file       = false
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.msur.certificate_authority.0.data)
-  host                   = data.aws_eks_cluster.msur.endpoint
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    command     = "aws-iam-authenticator"
-    args        = ["token", "-i", "${data.aws_eks_cluster.msur.name}"]
+    args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
   }
 }
 
-/*
 provider "helm" {
   kubernetes {
     load_config_file       = false
@@ -44,7 +21,29 @@ provider "helm" {
     }
   }
 }
-*/
+
+
+/*
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_eks_cluster" "msur" {
+  name = var.kubernetes_cluster_id
+}
+
+provider "kubernetes" {
+  load_config_file       = false
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.msur.certificate_authority.0.data)
+  host                   = data.aws_eks_cluster.msur.endpoint
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws-iam-authenticator"
+    args        = ["token", "-i", "${data.aws_eks_cluster.msur.name}"]
+  }
+}
+
+
 
 provider "helm" {
   kubernetes {
@@ -58,6 +57,7 @@ provider "helm" {
     }
   }
 }
+*/
 
 resource "kubernetes_namespace" "argo-ns" {
   metadata {
