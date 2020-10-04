740cb5e0a8a89f99541003a2c0c64a3a06d5496a
mitraman <ronnie.mitra@gmail.com>
2020-10-04 08:37:10 +0100

    using a data block

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+  description = "AWS region ID for deployment (e.g. eu-west-1)"
+  type        = string
+  default     = "eu-west-2"
+}
+
 variable "kubernetes_cluster_id" {
   type = string
 }
